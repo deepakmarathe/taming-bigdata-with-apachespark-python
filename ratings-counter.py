b535e32fd58e876af0aa6be3bc8605e6077327d1
Deepak Marathe <mmt7034@MMT-MAC-7034.local>
2017-10-13 17:56:54 +0530

    program to compute movie ratings histogram from the movie rating dataset:

---
+++ ratings-counter.py
@@ -0,0 +1,14 @@
+from pyspark import SparkConf, SparkContext
+import collections
+
+conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+sc = SparkContext(conf = conf)
+
+dataset = "/Users/mmt7034/code/taming-bigdata-with-apachespark-python/ml-100k/u.data"
+lines = sc.textFile(dataset)
+ratings = lines.map(lambda x: x.split()[2])
+result = ratings.countByValue()
+
+sortedResults = collections.OrderedDict(sorted(result.items()))
+for key, value in sortedResults.items():
+    print("%s %i" % (key, value))
