507413af3fd1e0bf7f16e3729372b0c09b6f3a6e
deepak marathe <dpkmarathe@gmail.com>
2017-10-16 22:17:57 +0530

    changing the type from string to integer

--- movies/popular-movies.py
+++ movies/popular-movies.py
@@ -4,7 +4,7 @@ conf = SparkConf().setMaster("local").setAppName("PopularMovies")
 sc = SparkContext(conf = conf)
 
 lines = sc.textFile("/Users/mmt7034/code/taming-bigdata-with-apachespark-python/ml-100k/u.data")
-movies = lines.map(lambda x : (x.split()[1], 1))
+movies = lines.map(lambda x : (int(x.split()[1]), 1))
 movieCounts = movies.reduceByKey(lambda x, y: x + y)
 
 results = movieCounts.map(lambda (x, y) : (y, x)).sortByKey().map(lambda (x, y) : (y, x)).collect()
