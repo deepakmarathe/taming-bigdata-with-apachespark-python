c500d2b6eb20d590fdeb215573ecbc99a2c36534
deepak marathe <dpkmarathe@gmail.com>
2017-10-16 21:49:08 +0530

    find the most popular movie by number of watches

---
+++ movies/popular-movies.py
@@ -0,0 +1,16 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("PopularMovies")
+sc = SparkContext(conf = conf)
+
+lines = sc.textFile("/Users/mmt7034/code/taming-bigdata-with-apachespark-python/ml-100k/u.data")
+movies = lines.map(lambda x : (x.split()[1], 1))
+movieCounts = movies.reduceByKey(lambda x, y: x + y)
+
+results = movieCounts.map(lambda (x, y) : (y, x)).sortByKey().map(lambda (x, y) : (y, x)).collect()
+
+for result in results : 
+    print result
+
+
+
